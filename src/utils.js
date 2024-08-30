6c30687e63ae1212eaf02ecfa9706f911911236b
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-08-30 16:39:31 +0200

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -6,7 +6,7 @@
  * the root directory of this source tree.
  */
 
-import * as pioNodeHelpers from 'platformio-node-helpers';
+import * as pioNodeHelpers from 'pioarduino-node-helpers';
 
 import os from 'os';
 import vscode from 'vscode';
